a1cb4fa51f28167fd3269db2d49636b8ea4dce01
Александр Вдовенко <Shumaher2014@mail.ru>
2023-04-12 19:55:32 +0400

    Add models for API

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    # review_id = models.ForeignKey(
+    #    Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ManyToManyField(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='reviews',
+        blank=True,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='reviews',
+        blank=True,
+        null=True
+    )
+
+
+class Review(models.Model):
+    text = models.TextField()
+    score = models.IntegerField(
+        choices=list(zip(range(1, 11), range(1, 11))),
+        unique=True)
